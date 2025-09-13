17ea359b6d3db941cde3216148927f02a32d07f1
DESKTOP-CTA1DA5\29610 <2961070026@qq.com>
2025-09-13 15:45:39 +0800

    no message

---
+++ 芯片文档/【正点原子】激光测距串口模块ATK-MS53L1M资料（新资料）/1，程序源码/ATK-MS53L1M模块IIC通信实验/探索者STM32F407开发板/Drivers/BSP/ATK_MS53L1M/atk_ms53l1m.c
@@ -0,0 +1,33 @@
+/**
+ ****************************************************************************************************
+ * @file        atk_ms53l1m.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2024-06-12
+ * @brief       ATK-MS53L1M模块驱动代码
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 探索者 F407开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ ****************************************************************************************************
+ */
+
+#include "./BSP/ATK_MS53L1M/atk_ms53l1m.h"
+#include "./SYSTEM/delay/delay.h"
+
+/**
+ * @brief       ATK-MS53L1M模块硬件初始化
+ * @param       无
+ * @retval      无
+ */
+void atk_ms53l1m_init(void)
+{
+    atk_ms53l1m_iic_init();
+    delay_ms(100);
+}
